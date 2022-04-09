268f7cd08e13827ec4fcfe1ffbeb3da551e0c6c9
Rza-Ismayil <rzaismail82@gmail.com>
2022-04-09 22:37:31 +0400

    Initial Commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Rza Ismayilov on 09.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
