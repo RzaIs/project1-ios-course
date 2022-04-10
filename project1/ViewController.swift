6381f1f6b08817dbcdd24de549cd60f26628b7ec
Rza-Ismayil <rzaismail82@gmail.com>
2022-04-10 23:29:47 +0400

    session1

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -11,9 +11,34 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		print("VIEW DID LOAD")
 		// Do any additional setup after loading the view.
+		self.test1(value: "hello world1")
+		self.test2("hello world2")
+		
+		self.view.backgroundColor = .green
+		let label = UILabel()
+		label.frame = CGRect.init(x: 150, y: 300, width: 100, height: 30)
+		label.text = "Hello World"
+		self.view.addSubview(label)
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		print("VIEW WILL APPEAR")
 	}
 
-
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		print(#function)
+	}
+	
+	func test1(value: String) {
+		print("test1 -> \(value)")
+	}
+	
+	func test2(_ value: String) {
+		print("test2 -> \(value)")
+	}
 }
 
