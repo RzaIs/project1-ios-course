fa2b6e12601ea45974c262f4c8af5ed807e320fd
Rza-Ismayil <rzaismail82@gmail.com>
2022-04-11 23:58:40 +0400

    session2

---
+++ project1/Vehicle.swift
@@ -0,0 +1,77 @@
+//
+//  Vehicle.swift
+//  project1
+//
+//  Created by Rza Ismayilov on 11.04.22.
+//
+
+import Foundation
+
+/*
+ Builder Creator example
+ */
+
+class Vehicle : CustomStringConvertible {
+	private var _fuelType : FuelType = .benzin
+	private var _nbOfPassenger : Int = 5
+	private var _nbOfWheels : Int = 4
+	
+	public var description: String {
+		return "Fuel type: \(fuelType == .diesel ? "diesel" : "benzin"), Number of passengers: \(nbOfPassenger), Number of wheels: \(nbOfWheels)."
+	}
+	
+	
+	public var fuelType : FuelType {
+		set {
+			_fuelType = newValue
+		}
+		get {
+			return _fuelType
+		}
+	}
+	
+	public var nbOfPassenger : Int {
+		set {
+			_nbOfPassenger = newValue
+		}
+		get {
+			return _nbOfPassenger
+		}
+	}
+	
+	public var nbOfWheels : Int {
+		set {
+			_nbOfWheels = newValue
+		}
+		get {
+			return _nbOfWheels
+		}
+	}
+}
+
+class VehicleBuilder {
+	private var vehicle : Vehicle = Vehicle()
+	
+	public func setFuelType(fuelType : FuelType) -> VehicleBuilder {
+		self.vehicle.fuelType = fuelType
+		return self
+	}
+	
+	public func setNbOfPassenger(nbOfPassenger : Int) -> VehicleBuilder {
+		self.vehicle.nbOfPassenger = nbOfPassenger
+		return self
+	}
+	
+	public func setNbOfWheels(nbOfWheels : Int) -> VehicleBuilder {
+		self.vehicle.nbOfWheels = nbOfWheels
+		return self
+	}
+	
+	public func buildVehicle() -> Vehicle {
+		return self.vehicle
+	}
+}
+
+enum FuelType {
+	case benzin, diesel
+}
