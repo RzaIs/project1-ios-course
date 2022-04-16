cda99d3e352fe04cd550dbb48b304331452544ba
Rza <rzaismail82@gmail.com>
2022-04-16 20:33:06 +0400

    session3

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -6,124 +6,73 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 	
-	var mutableData : String =  "Hello World"
+//    private var vm = MainViewModel()
+    
+    private lazy var redView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        
+        return view
+    }()
+    
+    private lazy var textLabel : UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.backgroundColor = .green
+        label.textColor = .black
+        label.textAlignment = .center
+        label.text = "HELLO WORLD"
+        return label
+    }()
+    
+    private lazy var contentText : UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.numberOfLines = 0
+        label.backgroundColor = .green
+        label.textColor = .black
+        label.textAlignment = .center
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        return label
+    }()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		print("VIEW DID LOAD")
 		// Do any additional setup after loading the view.
-		self.test1(value: "hello world1")
-		self.test2("hello world2")
-		
-		self.view.backgroundColor = .green
-		let label = UILabel()
-		label.frame = CGRect.init(x: 150, y: 300, width: 100, height: 30)
-		label.textColor = .red
-		label.text = "Hello World"
-		self.view.addSubview(label)
-		
-		self.processData()
-		print(self.mutableData)
-		
-		self.mutableData = self.pureProcessData()
-		print(self.mutableData)
-		
-		
-		
-		let data1 : SingleData = SingleData.getObj()
-		let data2 : SingleData = SingleData.getObj()
-		let data3 : SingleData = SingleData.getObj()
-		
-		data2.setInfo(newInfo: "This is the data of data 2\n")
-		print(data1.getInfo(), data2.getInfo(), data3.getInfo())
-		
-        let vehicle : Vehicle = Vehicle.VehicleBuilder()
-			.setFuelType(fuelType: .diesel)
-			.setNbOfPassenger(nbOfPassenger: 8)
-			.setNbOfWheels(nbOfWheels: 6)
-			.buildVehicle()
-		
-		print(vehicle.description)
-		
-		let animalCreateor : AnimalCreateor = AnimalCreateor()
-		let cat: Animal = animalCreateor.creatAnimal(isDog: false)
-		let dog: Animal = animalCreateor.creatAnimal(isDog: true)
-		cat.makeNoise()
-		dog.makeNoise()
+        self.view.backgroundColor = .blue
         
-        let protobase = ProtoClassBase(data1: "data1", data2: "data2")
-        let protobaseClone = ProtoClassBase(obj: protobase)
+        self.view.addSubview(self.redView)
         
-        let proto = ProtoClass(baseObj: protobase, data3: "data3")
-        let protoClone = ProtoClass(obj: proto)
+        self.redView.snp.makeConstraints { make in
+            make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).offset(20)
+            make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right).offset(-20)
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(-20)
+        }
         
-        print("protobase", protobase.data1, protobase.data2, protobase)
-        print("protobaseClone", protobaseClone.data1, protobaseClone.data2, protoClone)
+        self.redView.addSubview(self.textLabel)
         
-        print("proto", proto.data1, proto.data2, proto.data3, proto)
-        print("protoClone", protoClone.data1, protoClone.data2, protoClone.data3, protoClone)
+        self.textLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(50)
+            make.right.equalToSuperview().offset(-50)
+            make.top.equalToSuperview().offset(20)
+//            make.bottom.equalToSuperview().offset(-20)
+        }
+        
+        self.redView.addSubview(self.contentText)
+        
+        self.contentText.snp.makeConstraints {make in
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.top.equalTo(self.textLabel.snp.bottom).offset(20)
+        }
+        
+    }
+	
 
-	}
-	
-	override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(animated)
-		print("VIEW WILL APPEAR")
-	}
-
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		print(#function)
-	}
-	
-	func test1(value: String) {
-		print("test1 -> \(value)")
-	}
-	
-	func test2(_ value: String) {
-		print("test2 -> \(value)")
-	}
-	
-	func processData() {
-		self.mutableData = "text1"
-	}
-	
-	func pureProcessData() -> String{
-		return "text2"
-	}
 }
 
-
-func MapFilterReduce() {
-    // list
-    let listInt = [1,2,3,4,5,6,7,8,9,0];
-    print(listInt)
-    
-    // map
-    let listStr = listInt.map({num in
-        return "\(num)"
-    })
-    print(listStr)
-    
-    // filter
-    let listOddNum = listInt.filter({num in
-        return num % 2 == 1
-    })
-    print(listOddNum)
-    
-    // reduce
-    let sum = listInt.reduce(0, {prev, num in
-        prev + num
-    })
-    print(sum)
-    
-    // reduce with filter
-    let sumOfOdd = listInt.filter({num in
-        return num % 2 == 1
-    }).reduce(0, {prev, num in
-        prev + num
-    })
-    print(sumOfOdd)
-}
